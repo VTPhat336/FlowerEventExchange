8e0d8d48c1d273da9915896ada06927cc39b300c
tinvtse161572 <tinvtse161572@fpt.edu.vn>
2024-10-29 17:57:35 +0700

    From push BE

---
+++ SWP391_G5_EventFlowerExchange/src/main/java/com/SWP391_G5_EventFlowerExchange/Swp391G5EventFlowerExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.SWP391_G5_EventFlowerExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391G5EventFlowerExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391G5EventFlowerExchangeApplication.class, args);
+	}
+
+}
---
+++ SWP391_G5_EventFlowerExchange/src/test/java/com/SWP391_G5_EventFlowerExchange/Swp391G5EventFlowerExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWP391_G5_EventFlowerExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391G5EventFlowerExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
